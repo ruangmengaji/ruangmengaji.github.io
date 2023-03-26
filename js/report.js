f7550c5ecaac26d692be3a032776604fb516198e
Mohd Hafiz <xmhafiz@gmail.com>
2023-03-26 14:44:49 +0800

    fix: firebase script

--- js/report.js
+++ js/report.js
@@ -19,3 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+let date = new Date();
+logEvent(analytics, 'landing', {
+    dateTime: date
+});
